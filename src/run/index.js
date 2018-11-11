d06c607b443e78e3561da7fd204226a003fd919f
ftreml <ftreml@gmail.com>
2018-11-11 17:52:21 +0000

    parse reporter options parameter

--- src/run/index.js
+++ src/run/index.js
@@ -1,6 +1,7 @@
 const util = require('util')
 const path = require('path')
 const Mocha = require('mocha')
+const isJSON = require('is-json')
 const debug = require('debug')('botium-cli-run')
 
 const outputTypes = [
@@ -12,8 +13,29 @@ const outputTypes = [
   'mochawesome'
 ]
 
+const parseReporterOptions = (args) => {
+  if (!args) return
+
+  if (isJSON(args)) {
+    return JSON.parse(args)
+  }
+
+  return args.split(',').reduce((acc, option) => {
+    if (option.indexOf('=') > 0) {
+      const [key, value] = option.split('=')
+      acc[key] = value
+    } else {
+      acc[option] = true
+    }
+    return acc
+  }, {})
+}
+
 const handler = (argv) => {
   debug(`command options: ${util.inspect(argv)}`)
+
+  const reporterOptions = parseReporterOptions(argv.reporterOptions)
+  debug(`Mocha Reporter "${argv.output}", options: ${util.inspect(reporterOptions)}`)
 
   argv.testsuitename = process.env.BOTIUM_TESTSUITENAME || argv.testsuitename
   argv.timeout = process.env.BOTIUM_TIMEOUT || argv.timeout
@@ -25,7 +47,7 @@ const handler = (argv) => {
 
   const mocha = new Mocha({
     reporter: argv.output,
-    reporterOptions: argv.reporterOptions
+    reporterOptions
   })
   mocha.addFile(path.resolve(__dirname, 'mocha.spec.js'))
 
@@ -56,7 +78,7 @@ module.exports = {
       default: 60
     })
     yargs.option('reporter-options', {
-      describe: 'Options for mocha reporter, forwared as-is'
+      describe: 'Options for mocha reporter, either as JSON, or as key-value pairs ("option1=value1,option2=value2,..."). For details see documentation of the used mocha reporter.'
     })
   },
   handler
