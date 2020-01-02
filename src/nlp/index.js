65fcf1001e8f31208dbb686ce84bb5f6b1b85219
tremlfl <florian.treml@drei.com>
2020-01-02 21:23:04 +0100

    BOT-1041 validate algorithm

--- src/nlp/index.js
+++ src/nlp/index.js
@@ -1,11 +1,15 @@
 const util = require('util')
+const yargsCmd = require('yargs')
 const fs = require('fs')
 const _ = require('lodash')
 const { BotDriver } = require('botium-core')
 const debug = require('debug')('botium-cli-nlp')
 
 const getConnector = (containermode) => {
-  if (containermode.startsWith('botium-connector')) {
+  if (!containermode) {
+    console.log('CONTAINERMODE capability empty, Botium connector cannot be loaded.')
+    process.exit(1)
+  } else if (containermode.startsWith('botium-connector')) {
     return require(containermode)
   } else {
     return require(`botium-connector-${containermode}`)
@@ -67,9 +71,204 @@ const handler = (argv) => {
   }
   const { ExtractIntentUtterances, TrainIntentUtterances, CleanupIntentUtterances } = pluginConnector.NLP
 
+  if (argv.algorithm === 'validate') {
+    // eslint-disable-next-line no-unexpected-multiline
+    (async () => {
+      argv.output = argv.output || 'validate.csv'
+      argv.outputPredictions = argv.outputPredictions || 'validate-predictions.csv'
+      if (!argv.train || !argv.test) {
+        return yargsCmd.showHelp()
+      }
+
+      compiler.ReadScriptsFromDirectory(argv.train)
+      const trainingIntents = Object.keys(compiler.utterances).map(intentName => ({
+        intentName,
+        utterances: compiler.utterances[intentName].utterances
+      }))
+      debug(`Read training intent utterances: ${JSON.stringify(trainingIntents.map(i => ({ intentName: i.intentName, utterances: i.utterances.length })), null, 2)}`)
+
+      const testCompiler = driver.BuildCompiler()
+      testCompiler.ReadScriptsFromDirectory(argv.test)
+      const testIntents = Object.keys(testCompiler.utterances).map(intentName => ({
+        intentName,
+        utterances: testCompiler.utterances[intentName].utterances
+      }))
+      debug(`Read test intent utterances: ${JSON.stringify(testIntents.map(i => ({ intentName: i.intentName, utterances: i.utterances.length })), null, 2)}`)
+
+      const predictionDetails = []
+      const validateMatrix = {}
+
+      console.log('Starting training.')
+      let trainResult = null
+      try {
+        trainResult = await TrainIntentUtterances({}, trainingIntents, {})
+      } catch (err) {
+        console.log(`Validation training failed: ${err.message}`)
+        return
+      }
+
+      console.log('Starting testing.')
+      try {
+        const intentPromises = testIntents.map(async (testIntent) => {
+          testIntent.predictions = {}
+
+          const intentDriver = new BotDriver(trainResult.caps)
+          const intentContainer = await intentDriver.Build()
+
+          for (const utt of testIntent.utterances) {
+            try {
+              await intentContainer.Start()
+
+              await intentContainer.UserSaysText(utt)
+              const botMsg = await intentContainer.WaitBotSays()
+
+              const predictedIntentName = _.get(botMsg, 'nlp.intent.name') || 'None'
+              const mappedIntentName = (trainResult.trainedIntents && (trainResult.trainedIntents.find(ti => ti.mapFromIntentName === predictedIntentName) || {}).intentName) || predictedIntentName
+
+              if (mappedIntentName) {
+                testIntent.predictions[mappedIntentName] = (testIntent.predictions[mappedIntentName] || 0) + 1
+              }
+
+              testIntent.techok = (testIntent.techok || 0) + 1
+              predictionDetails.push({
+                match: (testIntent.intentName === mappedIntentName) ? 'Y' : 'N',
+                utterance: utt,
+                expectedIntent: testIntent.intentName,
+                predictedIntent: predictedIntentName
+              })
+
+              await intentContainer.Stop()
+            } catch (err) {
+              testIntent.techfailures = (testIntent.techfailures || 0) + 1
+              predictionDetails.push({
+                match: 'N',
+                utterance: utt,
+                expectedIntent: testIntent.intentName,
+                predictedIntent: null
+              })
+
+              console.log(`Validate: Failed sending utterance "${utt}" - ${err.message}`)
+            }
+          }
+          await intentContainer.Clean()
+        })
+        await Promise.all(intentPromises)
+
+        const expectedIntents = {}
+        for (const testIntent of testIntents) {
+          expectedIntents[testIntent.intentName] = testIntent.predictions
+        }
+        const allIntentNames = testIntents.map(fi => fi.intentName).concat(['None'])
+        for (const intentName of allIntentNames) {
+          expectedIntents[intentName] = expectedIntents[intentName] || { }
+        }
+
+        const matrix = []
+        for (const testIntent of testIntents) {
+          const totalPredicted = allIntentNames.reduce((agg, otherIntentName) => {
+            return agg + (expectedIntents[otherIntentName][testIntent.intentName] || 0)
+          }, 0)
+          const totalExpected = allIntentNames.reduce((agg, otherIntentName) => {
+            return agg + (testIntent.predictions[otherIntentName] || 0)
+          }, 0)
+
+          const score = {
+            techok: testIntent.techok || 0,
+            techfailures: testIntent.techfailures || 0
+          }
+
+          if (totalPredicted === 0) {
+            score.precision = 0
+          } else {
+            score.precision = (testIntent.predictions[testIntent.intentName] || 0) / totalPredicted
+          }
+          if (totalExpected === 0) {
+            score.recall = 0
+          } else {
+            score.recall = (testIntent.predictions[testIntent.intentName] || 0) / totalExpected
+          }
+          if (score.precision === 0 && score.recall === 0) {
+            score.F1 = 0
+          } else {
+            score.F1 = 2 * ((score.precision * score.recall) / (score.precision + score.recall))
+          }
+
+          matrix.push({
+            intent: testIntent.intentName,
+            predictions: testIntent.predictions,
+            score
+          })
+        }
+
+        Object.assign(validateMatrix, {
+          techok: matrix.reduce((sum, r) => sum + r.score.techok, 0),
+          techfailures: matrix.reduce((sum, r) => sum + r.score.techfailures, 0),
+          precision: matrix.reduce((sum, r) => sum + r.score.precision, 0) / matrix.length,
+          recall: matrix.reduce((sum, r) => sum + r.score.recall, 0) / matrix.length,
+          matrix
+        })
+        if (validateMatrix.precision === 0 && validateMatrix.recall === 0) {
+          validateMatrix.F1 = 0
+        } else {
+          validateMatrix.F1 = 2 * ((validateMatrix.precision * validateMatrix.recall) / (validateMatrix.precision + validateMatrix.recall))
+        }
+      } catch (err) {
+        console.log(`Validate testing failed: ${err.message}`)
+      } finally {
+        try {
+          await CleanupIntentUtterances({}, trainResult)
+        } catch (err) {
+          console.log(`Validate training failed: ${err.message}`)
+        }
+      }
+
+      console.log('############# Summary #############')
+      console.log(`Validate: Precision=${validateMatrix.precision.toFixed(4)} Recall=${validateMatrix.recall.toFixed(4)} F1-Score=${validateMatrix.F1.toFixed(4)} Tech.OK=${validateMatrix.techok} Tech.Failures=${validateMatrix.techfailures}`)
+
+      const csvLines = [
+        ['intent', 'precision', 'recall', 'F1', 'Tech.OK', 'Tech.Failures'].join(';')
+      ]
+      for (let m = 0; m < validateMatrix.matrix.length; m++) {
+        const matrix = validateMatrix.matrix[m]
+        csvLines.push([
+          matrix.intent,
+          matrix.score.precision.toFixed(4),
+          matrix.score.recall.toFixed(4),
+          matrix.score.F1.toFixed(4),
+          matrix.score.techok,
+          matrix.score.techfailures
+        ].join(';'))
+      }
+      try {
+        fs.writeFileSync(argv.output, csvLines.join('\r\n'))
+        console.log(`Wrote output file ${argv.output}`)
+      } catch (err) {
+        console.log(`Failed to write output file ${argv.output} - ${err.message}`)
+      }
+
+      const csvLinesPredictions = [
+        ['match', 'utterance', 'expectedIntent', 'predictedIntent'].join(';')
+      ].concat(predictionDetails.map(d => [
+        d.match,
+        d.utterance || '',
+        d.expectedIntent || '',
+        d.predictedIntent || ''
+      ].join(';')
+      ))
+      try {
+        fs.writeFileSync(argv.outputPredictions, csvLinesPredictions.join('\r\n'))
+        console.log(`Wrote predictions output file ${argv.outputPredictions}`)
+      } catch (err) {
+        console.log(`Failed to write predictions output file ${argv.outputPredictions} - ${err.message}`)
+      }
+    })()
+  }
   if (argv.algorithm === 'k-fold') {
     // eslint-disable-next-line no-unexpected-multiline
     (async () => {
+      argv.output = argv.output || 'k-fold.csv'
+      argv.outputPredictions = argv.outputPredictions || 'k-fold-predictions.csv'
+
       let extractedIntents = null
       if (argv.extract) {
         try {
@@ -302,8 +501,8 @@ module.exports = {
   describe: 'Run Botium NLP Analytics',
   builder: (yargs) => {
     yargs.positional('algorithm', {
-      describe: 'NLP Analytics Algorithm to use',
-      choices: ['k-fold']
+      describe: 'NLP Analytics Algorithm to use. "validate" - train with training set, evaluate with test set. "k-fold" - k-fold monte-carlo cross-validation with full data set',
+      choices: ['validate', 'k-fold']
     })
     yargs.option('k', {
       describe: 'K for K-Fold',
@@ -316,7 +515,7 @@ module.exports = {
       default: true
     })
     yargs.option('extract', {
-      describe: 'extract utterances from connector workspace (otherwise load from --convos directory or .)',
+      describe: 'Extract utterances from connector workspace (otherwise load from --convos directory or .) - k-fold only.',
       boolean: true,
       default: false
     })
@@ -324,12 +523,16 @@ module.exports = {
       describe: 'Only evaluate for the given intents'
     })
     yargs.option('output', {
-      describe: 'Output scores to CSV file',
-      default: 'k-fold.csv'
+      describe: 'Output scores to CSV file (default validate.csv or k-fold.csv)'
     })
     yargs.option('outputPredictions', {
-      describe: 'Output intent predictions to CSV file',
-      default: 'k-fold-predictions.csv'
+      describe: 'Output intent predictions to CSV file (default validate-predictions.csv or k-fold-predictions.csv)'
+    })
+    yargs.option('train', {
+      describe: 'Folder holding the training set - validate only, required.'
+    })
+    yargs.option('test', {
+      describe: 'Folder holding the test set - validate only, required.'
     })
   },
   handler,
